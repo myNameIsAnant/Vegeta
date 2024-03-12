0d9a0de00cb5dc47fc2c1f9d92fb9a8df6431924
darekillr <anant2011puri@gmail.com>
2024-03-12 15:42:46 +0530

    Find Divisors Solution Added

---
+++ getDivisors.py
@@ -0,0 +1,9 @@
+def getFactors(num: int) -> list:
+    factor_list = []
+    for x in range(1, (int(num**0.5)) + 1):
+        if num % x == 0:
+            factor_list.append(x)
+            if x != num // x:
+                factor_list.append(num // x)
+    factor_list.sort()
+    return factor_list
