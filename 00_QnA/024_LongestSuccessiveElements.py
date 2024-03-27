70eb6a824309179e0a8eb41f0ca6dfa3ef10447e
darekillr <anant2011puri@gmail.com>
2024-03-28 02:01:13 +0530

    DSA Daily Practice

--- 00_QnA/024_LongestSuccessiveElements.py
+++ 00_QnA/024_LongestSuccessiveElements.py
@@ -38,7 +38,7 @@ def longestSuccessiveElements(a: List[int]) -> int:
             ct += 1
         max_ct = max(max_ct, ct)
         ct = 0
-    return max(max_ct, ct)
+    return max_ct
 
 
 array = [15, 6, 2, 1, 16, 4, 2, 29, 9, 12, 8, 5, 14, 21, 8, 12, 17, 16, 6, 26, 3]
