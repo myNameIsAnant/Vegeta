a266efc1183803627e96cd36379baa8f02ed894a
darekillr <anant2011puri@gmail.com>
2024-04-02 19:20:32 +0530

    Practicing DSA Questions

--- 00_QnA/040_CeilTheFloor.py
+++ 00_QnA/040_CeilTheFloor.py
@@ -7,21 +7,21 @@ from typing import List
 
 
 def getFloorAndCeil(a: List[int], n: int, x: int) -> List[int]:
-    floor = -1
-    ceil = -1
+    f = -1
+    c = -1
     low = 0
     high = n - 1
     while low <= high:
         mid = (low + high) // 2
         if a[mid] == x:
-            return x, x
+            return a[mid], a[mid]
         elif a[mid] > x:
-            ceil = a[mid]
+            c = a[mid]
             high = mid - 1
         else:
-            floor = a[mid]
+            f = a[mid]
             low = mid + 1
-    return floor, ceil
+    return f, c
 
 
 array = [3, 4, 4, 7, 8, 10]
