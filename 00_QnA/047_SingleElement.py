225c398c8c9664b6f719f5184cb339747f9d0a5b
darekillr <anant2011puri@gmail.com>
2024-04-06 10:28:10 +0530

    New Aprroach Added

--- 00_QnA/047_SingleElement.py
+++ 00_QnA/047_SingleElement.py
@@ -7,24 +7,46 @@ from typing import List
 
 
 def singleNonDuplicate(nums: List[int]) -> int:
+
+    ### ***** Approach 4 ***** ###
+    n = len(nums) - 1
+    if len(nums) == 1 or nums[0] != nums[1]:
+        return nums[0]
+    if nums[-1] != nums[-2]:
+        return nums[-1]
     low = 0
     high = len(nums) - 1
     while low <= high:
-        mid = (low + high) // 2
-        if low == high:
-            return nums[low]
+        mid = low + ((high - low) // 2)
         if nums[mid] != nums[mid - 1] and nums[mid] != nums[mid + 1]:
             return nums[mid]
-        if nums[mid] == nums[mid - 1]:
-            if mid % 2 == 1:
-                low = mid + 1
-            else:
-                high = mid - 1
-        elif nums[mid] == nums[mid + 1]:
-            if mid % 2 == 1:
+        else:
+            if (nums[mid] == nums[mid - 1] and ((n - mid) % 2) == 0) or (
+                nums[mid] == nums[mid + 1] and ((n - mid) % 2) == 1
+            ):
                 high = mid - 1
             else:
                 low = mid + 1
+
+    ### ***** Approach 3 ***** ###
+    # low = 0
+    # high = len(nums) - 1
+    # while low <= high:
+    #     mid = (low + high) // 2
+    #     if low == high:
+    #         return nums[low]
+    #     if nums[mid] != nums[mid - 1] and nums[mid] != nums[mid + 1]:
+    #         return nums[mid]
+    #     if nums[mid] == nums[mid - 1]:
+    #         if mid % 2 == 1:
+    #             low = mid + 1
+    #         else:
+    #             high = mid - 1
+    #     elif nums[mid] == nums[mid + 1]:
+    #         if mid % 2 == 1:
+    #             high = mid - 1
+    #         else:
+    #             low = mid + 1
 
     ### ***** Approach 2 ***** ###
 
@@ -47,5 +69,5 @@ def singleNonDuplicate(nums: List[int]) -> int:
     # return curel
 
 
-array = [0, 1, 1, 2, 2, 5, 5]
+array = [1, 1, 2, 3, 3, 4, 4, 5, 5]
 print(singleNonDuplicate(array))
